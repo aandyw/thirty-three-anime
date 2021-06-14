ab0ebbed48f85423df4fe55d5758caefb4900db4
Pie31415 <andy999coc@gmail.com>
2021-06-13 21:34:04 -0400

    final commit

--- anime33/anime33/settings.py
+++ anime33/anime33/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [] # add in host
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -118,19 +118,11 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-if not os.path.exists(STATIC_ROOT):
-    os.makedirs(STATIC_ROOT)
-
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'myapp/static'),
 )
